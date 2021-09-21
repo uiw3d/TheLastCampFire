a2641f7e4d590d334d10a26e093e30aebd085f7b
Li <jili1@uiwtx.edu>
2021-09-21 15:38:33 -0500

    able to move player with wsad, no gravity

--- Assets/Prefabs/Player.cs
+++ Assets/Prefabs/Player.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
+    [SerializeField] float WalkingSpeed = 5f;
     InputActions inputActions;
     Vector2 MoveInput;
+    Vector3 Velocity;
+    CharacterController characterController;
     private void Awake()
     {
         inputActions = new InputActions(); 
@@ -21,8 +24,10 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        characterController = GetComponent<CharacterController>();
         inputActions.Gameplay.Move.performed += MoveInputUpdated;
         inputActions.Gameplay.Move.canceled += MoveInputUpdated;
+       
     }
 
     void MoveInputUpdated(InputAction.CallbackContext ctx)
@@ -33,6 +38,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log($"player move input is: {MoveInput}");
+        Velocity = GetPlayerDesiredMoveDir() * WalkingSpeed;
+        characterController.Move(Velocity * Time.deltaTime);
+    }
+
+    Vector3 GetPlayerDesiredMoveDir()
+    {
+        return new Vector3(-MoveInput.y, 0f, MoveInput.x).normalized;
     }
 }
